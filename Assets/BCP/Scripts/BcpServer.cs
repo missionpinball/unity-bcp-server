40fe34a00395d23e671ecc726579ebea70092234
Quinn Capen <qcapen@gmail.com>
2015-10-23 18:37:50 -0600

    Added support for new external show commands

--- Assets/BCP/Scripts/BcpServer.cs
+++ Assets/BCP/Scripts/BcpServer.cs
@@ -20,7 +20,7 @@ public class BcpServer : MonoBehaviour
     /// <summary>
     /// The BCP Server version
     /// </summary>
-    public const string CONTROLLER_VERSION = "1.02-dev3";
+    public const string CONTROLLER_VERSION = "1.02-dev4";
 
     /// <summary>
     /// The BCP Server name
