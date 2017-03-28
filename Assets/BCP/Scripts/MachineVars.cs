d64a10eab659279696403e2cd234992dd7f8126d
Quinn Capen <qcapen@gmail.com>
2017-03-27 22:12:59 -0600

    Added player and machine variable stores

---
+++ Assets/BCP/Scripts/MachineVars.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+
+/// <summary>
+/// Manages machine variables from the pin controller.
+/// </summary>
+public class MachineVars
+{
+    private BcpVariableDictionary _variables;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MachineVars"/> class.
+    /// </summary>
+    public MachineVars()
+    {
+        _variables = new BcpVariableDictionary();
+    }
+
+    /// <summary>
+    /// Gets or sets the <see cref="BcpVariable"/> with the specified key.
+    /// </summary>
+    /// <value>
+    /// The <see cref="BcpVariable"/>.
+    /// </value>
+    /// <param name="key">The key.</param>
+    /// <returns></returns>
+    public BcpVariable this[string key]
+    {
+        get { return (BcpVariable)this._variables[key]; }
+
+        set { this._variables[key] = value; }
+    }
+
+    /// <summary>
+    /// Removes all variables from the machine variables store.
+    /// </summary>
+    public void Clear()
+    {
+        this._variables.Clear();
+    }
+
+    /// <summary>
+    /// Adds the specified key.
+    /// </summary>
+    /// <param name="key">The key.</param>
+    /// <param name="variable">The variable.</param>
+    public void Add(string key, BcpVariable variable)
+    {
+        this._variables.Add(key, variable);
+    }
+
+    /// <summary>
+    /// Adds the specified key.
+    /// </summary>
+    /// <param name="key">The key.</param>
+    /// <param name="value">The value.</param>
+    public void Add(string key, string value)
+    {
+        if (this._variables.Contains(key))
+            this._variables[key].AssignFromBcpParameterString(value);
+        else
+            this._variables.Add(key, new BcpVariable(value));
+    }
+
+    /// <summary>
+    /// Determines whether the dictionary contains the specified key.
+    /// </summary>
+    /// <param name="key">The key.</param>
+    /// <returns></returns>
+    public bool Contains(string key)
+    {
+        return this._variables.Contains(key);
+    }
+
+    /// <summary>
+    /// Removes the specified key.
+    /// </summary>
+    /// <param name="key">The key.</param>
+    public void Remove(string key)
+    {
+        this._variables.Remove(key);
+    }
+
+    /// <summary>
+    /// Gets the dictionary keys.
+    /// </summary>
+    /// <value>
+    /// The keys.
+    /// </value>
+    public ICollection Keys
+    {
+        get { return this._variables.Keys; }
+    }
+
+}
---
+++ Assets/BCP/Scripts/PlayerVars.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+
+/// <summary>
+/// Manages player variables from the pin controller.
+/// </summary>
+public class PlayerVars
+{
+    private BcpVariableDictionary _variables;
+
+    /// <summary>
+    /// Gets the static singleton object instance.
+    /// </summary>
+    /// <value>
+    /// The instance.
+    /// </value>
+	public static PlayerVars Instance { get; private set; }
+
+    /// <summary>
+    /// Called when the script instance is being loaded.
+    /// </summary>
+    public void Awake()
+    {
+        // Save a reference to the BcpServer component as our singleton instance
+        Instance = this;
+        _variables = new BcpVariableDictionary();
+    }
+
+    /// <summary>
+    /// Gets or sets the <see cref="BcpVariable"/> with the specified key.
+    /// </summary>
+    /// <value>
+    /// The <see cref="BcpVariable"/>.
+    /// </value>
+    /// <param name="key">The key.</param>
+    /// <returns></returns>
+    public BcpVariable this[string key]
+    {
+        get { return (BcpVariable)this._variables[key]; }
+
+        set { this._variables[key] = value; }
+    }
+
+    /// <summary>
+    /// Adds the specified key.
+    /// </summary>
+    /// <param name="key">The key.</param>
+    /// <param name="variable">The variable.</param>
+    public void Add(string key, BcpVariable variable)
+    {
+        this._variables.Add(key, variable);
+    }
+
+    /// <summary>
+    /// Determines whether the dictionary contains the specified key.
+    /// </summary>
+    /// <param name="key">The key.</param>
+    /// <returns></returns>
+    public bool Contains(string key)
+    {
+        return this._variables.Contains(key);
+    }
+
+    /// <summary>
+    /// Gets the dictionary keys.
+    /// </summary>
+    /// <value>
+    /// The keys.
+    /// </value>
+    public ICollection Keys
+    {
+        get { return this._variables.Keys; }
+    }
+
+
+}
